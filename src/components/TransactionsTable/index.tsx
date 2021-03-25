c64b93160b814ccba3bcac9784bcc3e01b327887
davi <manelv8@gmail.com>
2021-03-25 16:49:08 -0300

    seed data and list transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import { Container } from './styles';
 
+interface Transaction{
+  id: number,
+  title: string,
+  amount: number,
+  type: string,
+  category: string,
+  createdAt: string
+}
+
+
 export function TransactionsTable() {
+
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(()=>{
     api.get('transactions')
-    .then(response => console.log(response.data))
+    .then(response => setTransactions(response.data.transactions))
   },[])
 
   return (
@@ -20,26 +33,14 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000</td>
-            <td>freelance</td>
-            <td>10/03/2021</td>
-          </tr>
-
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ -12.000</td>
-            <td>freelance</td>
-            <td>10/03/2021</td>
-          </tr>
-
-          <tr>
-            <td>aluguel</td>
-            <td className="withdraw">R$ -12.000</td>
-            <td>freelance</td>
-            <td>10/03/2021</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,43 @@ import { createServer, Model } from 'miragejs';
 import App from "./App";
 
 createServer({
-  models:{
+  models: {
     transaction: Model,
   },
-  routes(){
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdrawn',
+          category: 'Casa',
+          amount: 2000,
+          createdAt: new Date('2021-02-01 09:00:00'),
+        },
+      ],
+    });
+  },
+  routes() {
     this.namespace = 'api';
-
     this.get('/transactions', () => {
-      return this.schema.all('transactions')
-    })
+      return this.schema.all('transaction');
+    });
 
-    this.post('/transactions', (schema, request)=>{
-      const data = JSON.parse(request.requestBody)
-      return schema.create('transaction',data)
-    })
-
-  }
-})
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
